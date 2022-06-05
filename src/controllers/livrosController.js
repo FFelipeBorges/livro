7b9ec06c9e9bb30693f6aab04e870654d669f56d
Felipe <fbf_felipeb@hotmail.com>
2022-06-05 16:13:37 -0300

    feat: atualizando um livro

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,3 +1,4 @@
+const { DESCRIBE } = require('sequelize/types/lib/query-types');
 const { Livro } = require('../models')
 
 const livrosController = {
@@ -5,7 +6,29 @@ const livrosController = {
         const livros = await Livro.findAll();
 
         return response.json(livros);
-    }
+    },
+    update: async (request, response) => {
+        const { idLivro } = request.params;
+        const payload = request.body;
+
+        // const livro = await Livro.update(payload, {
+        //     where: {
+        //         id: idLivro
+        //     },
+        // });
+
+        // const livro = await Livro.findOne({where: { id: idLivro }});
+
+        //**Outra atualiza os paramentros que foram modificados */
+
+        const livroAtualizado = await Livro.findOne({where: { id: idLivro }});
+        
+        Object.assign(payload, livro);
+
+        await livro.save()
+
+        return response.json(livro);
+    },
 };
 
 module.exports = livrosController;
--- src/routes/livros.js
+++ src/routes/livros.js
@@ -3,5 +3,7 @@ const livrosController = require('../controllers/livrosController');
 const router = express.Router();
 
 router.get('/', livrosController.index);
+router.put('/:idLivro', livrosController.update);
+router.put('/:idLivro', livrosController.update);
 
 module.exports = router;
